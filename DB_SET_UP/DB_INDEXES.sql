f3649cb036055805abf005467e1e65ee7c67e371
SpyrosGiannopoulos <106885905+SpyrosGiannopoulos@users.noreply.github.com>
2022-06-05 22:07:49 +0300

    Delete DB_INDEXES.sql

--- DB_SET_UP/DB_INDEXES.sql
+++
@@ -1,6 +0,0 @@
-USE DB_PROJECT;
-
-#CREATE INDEX idx_start_date ON PROJECTS(start_date);
-#CREATE INDEX idx_due_date ON PROJECTS(due_date);
-#CREATE INDEX idx_title ON PROJECTS(title);
-#SHOW INDEX FROM PROJECTS;
